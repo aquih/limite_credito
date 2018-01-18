16b6fcdddfcc9537c9311f76d033c7cc25d7b640
Rodrigo Fernandez <rodrigo@solucionesprisma.com>
2018-01-18 16:24:29 +0000

    Quitar archivo de demo que no existe

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,8 @@
     'name': "limite_credito",
 
     'summary': """
-        Modulo que lleva el control del limite de credito""",
+        Modulo que lleva el control del limite de credito
+    """,
 
     'description': """
         Modulo que lleva el control del limite de credito
@@ -12,22 +13,14 @@
     'author': "Rodolfo Borstcheff",
     'website': "http://www.aquih.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
-    # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
 
-    # any module necessary for this one to work correctly
     'depends': ['base','sale'],
 
-    # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/sale_views.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+
 }
