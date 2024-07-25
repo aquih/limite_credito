5ed22640678c8bc7c769496a616f31efc88022b8
Rodrigo Fernández <rodrigo@aquih.com>
2024-07-25 20:41:19 +0000

    Version 17.0

--- __manifest__.py
+++ __manifest__.py
@@ -1,22 +1,19 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "limite_credito",
+    'name': "Límite de Crédito desde Pedidos de Venta",
 
     'summary': """
-        Modulo que lleva el control del limite de credito""",
-
-    'description': """
-        Modulo que lleva el control del limite de credito
+        Modulo que lleva el control del limite de credito desde pedidos de venta.
     """,
-
-    'author': "Rodolfo Borstcheff",
+    'description': """
+        Modulo que lleva el control del limite de credito desde pedidos de venta.
+    """,
+    'author': "aquíH",
     'website': "http://www.aquih.com",
-
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    'depends': ['base','sale'],
-
+    'category': 'Sales/Sales',
+    'license': 'Other OSI approved licence',
+    'version': '1.0',
+    'depends': ['base', 'sale'],
     'data': [
         'views/partner_views.xml',
     ],
--- models/sale.py
+++ models/sale.py
@@ -10,25 +10,20 @@ import logging
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-
     def _limite_credito(self):
         if self.partner_id.credit_limit > 0 and self.partner_id.credit + self.amount_total > self.partner_id.credit_limit + self.partner_id.extra_financiamiento:
-            raise UserError(_('No puede sobrepasar el límite de crédito'))
+            raise UserError(_('No puede sobrepasar el límite de crédito.'))
 
     def _facturas_vencidas(self):
-        hoy = datetime.datetime.today()
+        hoy = datetime.date.today()
         if self.partner_id.dias_gracia > 0:
-            fecha_vencimiento = datetime.date(hoy.year, hoy.month, hoy.day) - datetime.timedelta(days=self.partner_id.dias_gracia)
+            fecha_vencimiento = hoy - datetime.timedelta(days=self.partner_id.dias_gracia)
         else:
-            fecha_vencimiento = datetime.date(hoy.year, hoy.month, hoy.day) - datetime.timedelta(days=1)
+            fecha_vencimiento = hoy - datetime.timedelta(days=1)
         
-        if version_info[0] == 14 or version_info[0] == 15:
-            facturas_vencidas = self.env['account.move'].search([('move_type', '=', 'out_invoice'), ('partner_id', '=', self.partner_id.id), ('state', '=', 'posted'), ('payment_state', '!=', 'paid'), ('payment_state', '!=', 'in_payment'), ('payment_state', '!=', 'reversed'), ('invoice_date_due', '<=', fecha_vencimiento)])
-        else:
-            facturas_vencidas = self.env['account.move'].search([('type', '=', 'out_invoice'), ('partner_id', '=', self.partner_id.id), ('state', '=', 'posted'), ('invoice_payment_state', '!=', 'paid'), ('invoice_date_due', '<=', fecha_vencimiento)])
-            
+        facturas_vencidas = self.env['account.move'].search([('move_type', '=', 'out_invoice'), ('partner_id', '=', self.partner_id.id), ('state', '=', 'posted'), ('payment_state', 'not in', ['paid', 'in_payment', 'reversed']), ('invoice_date_due', '<=', fecha_vencimiento)])            
         if facturas_vencidas:
-            raise UserError(_('El cliente tiene facturas vencidas'))
+            raise UserError(_('El cliente tiene facturas vencidas.'))
     
     def action_confirm(self):
         if not self.user_has_groups('sales_team.group_sale_manager'):
